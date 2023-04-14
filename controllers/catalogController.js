af469526a0fc513d1fabda45cea58a50c5b2c386
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-14 22:15:37 +0300

    Render catalog page

---
+++ controllers/catalogController.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('catalog');
+});
+
+router.get('/:id', (req, res) => {
+    res.render('details');
+})
+module.exports = router;
