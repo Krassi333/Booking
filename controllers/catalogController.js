83b2b7ed87ee31cd7463cab096467ee6459a2cd7
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-17 22:32:08 +0300

    Add search

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -3,9 +3,15 @@ const { getAll, getById } = require('../services/acomodation');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    const data = getAll();
+    const search = req.query.search || '';
+    const city = req.query.city || '';
+    const fromPrice = Number(req.query.fromPrice) || 1;
+    const toPrice = Number(req.query.toPrice) || 1000;
+console.log(search,city,fromPrice,toPrice);
+    const data = getAll(search);
     console.log(data);
-    res.render('catalog', { data });
+
+    res.render('catalog', { data, search, city, fromPrice, toPrice });
 });
 
 router.get('/:id', (req, res) => {
