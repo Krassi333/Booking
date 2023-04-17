b86032ca557d244415341fd44c80002b6c7391c3
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-17 21:03:38 +0300

    Add home controller

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const homeController = require('./controllers/homeController');
 
 const app = express();
 
@@ -11,4 +12,5 @@ app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: false }));
 app.use('/static', express.static('static'));
 
+app.use(homeController);
 app.listen(3000, () => { console.log('Server is listening on port 3000...'); });
