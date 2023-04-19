25293667ab05ddf260e61402eff71cbcdf0f41df
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-19 13:49:37 +0300

    Use configurations in index.js

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 const express = require('express');
-const hbs = require('express-handlebars');
-const homeController = require('./controllers/homeController');
-const defaultController = require('./controllers/defaultController');
-const catalogController = require('./controllers/catalogController');
-const createController = require('./controllers/createController');
+const expressConfig = require('./config/express');
+const routsConfig = require('./config/routes');
+const databaseConfig = require('./config/database');
 
+async function start() {
+    const app = express();
+    expressConfig(app);
+    routsConfig(app);
+    await databaseConfig(app);
 
-const app = express();
+    app.listen(3000, () => { console.log('Server is listening on port 3000...'); });
+}
 
-app.engine('hbs', hbs.engine({
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+start();
 
-app.use(express.urlencoded({ extended: false }));
-app.use('/static', express.static('static'));
-
-app.use(homeController);
-app.use('/catalog', catalogController);
-app.use('/create', createController);
-
-app.all('*', defaultController);
-
-app.listen(3000, () => { console.log('Server is listening on port 3000...'); });
