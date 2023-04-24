3c4643f7cc744b3398f4ecdc630f2bf724fd5222
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-24 21:49:36 +0300

    Add register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
-const { login } = require('../services/authService');
+const { login, register } = require('../services/authService');
 
 const secret = 'gsdfghjkl';
 
@@ -9,10 +9,15 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    const result = await login(req.body.username, req.body.password);
-    const token = req.signJwt(result);
-    res.cookie('token', token, { maxAge: 14400000 });
-    res.redirect('/')
+    try {
+        const result = await login(req.body.username, req.body.password);
+        attachToken(req, res, result);
+        res.redirect('/')
+    } catch (err) {
+        console.log('post login ' + err);
+        res.render('register', { error: err.message.split('/n') })
+    }
+
 })
 
 router.get('/register', (req, res) => {
@@ -20,11 +25,34 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    const result = await login(req.body.username, req.body.password);
-    const token = req.signJwt(result);
-    res.cookie('token', token, { maxAge: 14400000 });
-    res.redirect('/')
+    try {
+        if (req.body.username.trim() == '' || req.body.password.trim() == '') {
+            throw new Error('All fields are required!');
+        }
+
+        if (req.body.password.trim() != req.body.repass.trim()) {
+            throw new Error('Passwords don\'t match');
+        }
+
+        const result = await register(req.body.username, req.body.password);
+        attachToken(req, res, result);
+        res.redirect('/');
+    } catch (err) {
+        console.log('post reg ' + err);
+
+        res.render('register', { error: err.message.split('/n') })
+    }
+
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    return res.redirect('/');
 })
 
+function attachToken(req, res, data) {
+    const token = req.signJwt(data);
+    res.cookie('token', token, { maxAge: 14400000 });
+}
 
 module.exports = router;
