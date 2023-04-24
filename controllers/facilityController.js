3a2efcf4364e90bcb5cf645e3901660aefb488cc
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-24 21:50:49 +0300

    Add validation of facility controller

--- controllers/facilityController.js
+++ controllers/facilityController.js
@@ -22,7 +22,12 @@ router.post('/create', async (req, res) => {
 router.get('/:roomId/decorateRoom', async (req, res) => {
     const roomId = req.params.roomId;
     const room = await getById(roomId);
-    const facilities = await getAllFacilities();
+    
+    if (!req.user || data.owner != req.user._id) {
+        return res.redirect('/auth/login');
+    }
+
+const facilities = await getAllFacilities();
 
     facilities.forEach(f => {
         if ((room.facilities || []).some(id => id.toString() == f._id.toString())) {
@@ -38,9 +43,16 @@ router.get('/:roomId/decorateRoom', async (req, res) => {
 });
 
 router.post('/:roomId/decorateRoom', async (req, res) => {
+    const roomId = req.params.roomId;
+    const room = await getById(roomId);
+    
+    if (!req.user || data.owner != req.user._id) {
+        return res.redirect('/auth/login');
+    }
+    
     await addFacilities(req.params.roomId, Object.keys(req.body));
 
-    res.redirect('/details/' + req.params.roomId);
+    res.redirect('/catalog/' + req.params.roomId);
 })
 
 module.exports = router;
