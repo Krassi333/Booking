7ce92f6a538f2ed5f61d97eaee1d038db56a3875
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-24 21:49:01 +0300

    Update routs

--- config/routes.js
+++ config/routes.js
@@ -4,13 +4,17 @@ const catalogController = require('../controllers/catalogController');
 const createController = require('../controllers/createController');
 const facilityController = require('../controllers/facilityController');
 const authController = require('../controllers/authController');
+const roomController = require('../controllers/roomController');
+
+const { hasUser } = require('../middlewares/guards');
 
 module.exports = (app) => {
     app.use(homeController);
     app.use('/catalog', catalogController);
-    app.use('/create', createController);
+    app.use('/create', hasUser(), createController);
     app.use('/facility', facilityController);
     app.use('/auth', authController);
+    app.use('/room', roomController);
 
     app.all('*', defaultController);
 }
