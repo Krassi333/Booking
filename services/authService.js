71a284778fb7d13b941961b18f00f808cb4b94b8
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-28 15:10:22 +0300

    Add authentication service

--- services/authService.js
+++ services/authService.js
@@ -1,15 +1,41 @@
-async function login(username, password) {
-    return new Promise((res, rej) => {
-        if (username.toLowerCase() == 'peter' && password == '123456') {
-            res({
-                _id: '234cba3456b43',
-                username: 'Peter',
-                roles: ['user']
-            });
-        } else {
-            rej(new Error('Incorrect username or password'));
-        }
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+async function register(username, password) {
+    // Check if the username is taken
+    const existing = await User.findOne({ username }).collation({ locale: 'en', strength: 2 }); //options i - case insansitive
+
+    if (existing) {
+        throw new Error('Username is taken.');
+    }
+    //hash password
+    const hashedPass = await bcrypt.hash(password, 10);
+    //create and save user
+    const user = await User.create({
+        username,
+        hashedPass
     })
+    //return user data
+    return {
+        _id: user._id,
+        username,
+        roles: user.roles
+    };
 }
 
-module.exports = { login }
+async function login(username, password) {
+    const user = await User.findOne({ username }).collation({ locale: 'en', strength: 2 }); //options i - case insansitive
+    const match = await bcrypt.compare(password, user.hashedPass);
+
+    if (!user || !match) {
+        throw new Error('Incorrect username or password');
+    }
+
+    return {
+        _id: user._id,
+        username: user.username,
+        roles: user.roles
+    };
+}
+
+module.exports = { login, register }
